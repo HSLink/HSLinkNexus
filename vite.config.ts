06eb35bfa4069705686e3caa5a27c94d02d2af37
yekai <2857693944@qq.com>
2025-01-11 17:16:41 +0800

    :construction_worker: ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // @ts-expect-error process is a nodejs global
@@ -7,6 +7,9 @@ const host = process.env.TAURI_DEV_HOST;
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [vue()],
+  build: {
+    target: ['edge90', 'chrome90', 'firefox90', 'safari15']
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
@@ -18,12 +21,12 @@ export default defineConfig(async () => ({
     strictPort: true,
     host: host || false,
     hmr: host
-      ? {
+        ? {
           protocol: "ws",
           host,
           port: 1421,
         }
-      : undefined,
+        : undefined,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
